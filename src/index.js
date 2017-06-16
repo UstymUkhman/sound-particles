d75f6cfe076554ecc5a69cf69bbf6f1f363eaa46
Ustym_Ukhman <ustym.ukhman@gmail.com>
2017-06-16 13:10:23 +0200

    Track Range Values Study added

--- src/index.js
+++ src/index.js
@@ -3,27 +3,14 @@ import Stats from 'three/examples/js/libs/stats.min';
 // import { TweenLite } from 'gsap';
 
 const analyser = require('web-audio-analyser');
+const MAX_DECIBELS = 255;
 
-/*
-  let max = 0
-
-  for (let i = 0; i < soundSource.analyser.frequencies().length; i++) {
-    max += (AnalyserNode.maxDecibels + i);
-  }
-
-  MAX_POWER = max / soundSource.analyser.frequencies().length - 1
-  MAX_POWER_RATIO = MAX_POWER / 100
-
- */
-
-// if analyser.frequencies().length === 1024 && AnalyserNode.maxDecibels === 255
-const MAX_POWER_RATIO = 765.5 / 100;
+//                             i   Hz        i      Hz          i       Hz
+// Theorical Frequency Range: (0 ~ 20) --> (800 ~ 20.000) --> (1024 ~ 25.600)
 
 export default class AudioReactive {
 
   constructor() {
-    // this._minPower = 5;
-
     this._startTime = null;
     this._pauseTime = null;
 
@@ -34,9 +21,6 @@ export default class AudioReactive {
 
     this._name = 'AudioReactive';
     this._audio = 'John Newman - Love Me Again';
-
-    // Song Range: 510.5 --> 621.5
-    // Theorical General Range: (0 ~ 20) --> (800 ~ 20.000) --> (1024 ~ 25.600)
   }
 
   init(element) {
@@ -45,29 +29,83 @@ export default class AudioReactive {
     }
 
     this._element = element;
-    this._context = element.getContext('2d');
-    this._context.fillStyle = '#FFFFFF';
+    this._context = this._element.getContext('2d');
 
     return true;
   }
 
-  startExperience() {
+  startExperience(study = false) {
+    let AudioContext = window.AudioContext || window.webkitAudioContext;
+    let audioContext = new AudioContext();
+    let audioContextAnalyser = audioContext.createAnalyser();
+
+    this._frequencyRange = audioContextAnalyser.frequencyBinCount;
+
+    this._getMaxAudioPower();
     this._experienceStarted = true;
-    this._initAudio();
+
+    if (study) {
+      this._getAudioValues();
+    } else {
+      this._loadAudioTrack();
+    }
   }
 
-  _initAudio() {
-    this._soundSource = document.createElement('audio');
+  _getAudioValues() {
+    this._maxFrequency = 0;
+    this._minFrequency = Infinity;
+
+    this._studingAudio = true;
+    this._loadAudioTrack(true);
+  }
+
+  _loadAudioTrack(study = false) {
+    let onAudioEnded = study ? this._setAudioValues.bind(this) : this._onAudioTrackEnded.bind(this);
+
+    if (this._soundSource === null) {
+      this._soundSource = document.createElement('audio');
+    }
 
     this._soundSource.autoplay = false;
     this._soundSource.src = 'assets/' + this._audio + '.mp3';
-    this._soundSource.addEventListener('ended', this._onAudioTrackEnded.bind(this));
+    this._soundSource.addEventListener('ended', onAudioEnded);
 
     this._soundSource.addEventListener('canplay', () => {
       this._soundSource.loaded = true;
       this._soundSource.volume = 1.0;
-      this._playAudioTrack();
+
+      if (!study) {
+        this.MAX_POWER = 7.655;
+        this.SONG_MIN_POWER = 510.5 / this.MAX_POWER;
+        this.SONG_MAX_POWER = 621.5 / this.MAX_POWER;
+        this.SONG_RANGE = this.SONG_MAX_POWER - this.SONG_MIN_POWER;
+
+        this._playAudioTrack();
+      } else {
+        this._soundSource.analyser = analyser(this._soundSource);
+        this._soundSource.play();
+
+        requestAnimationFrame(this._studyAudio.bind(this));
+      }
     });
+  }
+
+  _studyAudio() {
+    if (!this._studingAudio) {
+      return;
+    }
+
+    let frequency = this._getAnalyserFrequencies();
+
+    if (this._maxFrequency < frequency) {
+      this._maxFrequency = frequency;
+    }
+
+    if (this._minFrequency > frequency) {
+      this._minFrequency = frequency;
+    }
+
+    requestAnimationFrame(this._studyAudio.bind(this));
   }
 
   _playAudioTrack() {
@@ -77,7 +115,6 @@ export default class AudioReactive {
     this._soundSource.play();
     this._isPlaying = true;
 
-    // TweenLite.to(this._startExperienceButton, 0.5, {opacity: 1, display: 'block'});
     requestAnimationFrame(this._render.bind(this));
   }
 
@@ -86,6 +123,10 @@ export default class AudioReactive {
   }
 
   _getAnalysedValue() {
+    return this._getAnalyserFrequencies() / this.MAX_POWER;
+  }
+
+  _getAnalyserFrequencies() {
     let freq = this._soundSource.analyser.frequencies();
     let sum = 0;
 
@@ -93,18 +134,20 @@ export default class AudioReactive {
       sum += freq[i] + i;
     }
 
-    // Force to min value as this._minPower:
-    // sum = Math.max(this._minPower, sum);
-
     sum = sum / freq.length - 1;
-    return sum / MAX_POWER_RATIO;
+    return sum;
   }
 
   _drawAudioAnalysis(value) {
+    value -= this.SONG_MIN_POWER;
+    value = value * 100 / this.SONG_RANGE;
+    this._context.clearRect(0, 0, this._width, this._height);
+
     let h = this._height / 100 * value;
-    let x = this._width / 2 - 100;
+    let x = this._width / 2 - 50;
     let y = this._height - h;
 
+    this._context.fillStyle = '#FFFFFF';
     this._context.fillRect(x, y, 100, h);
   }
 
@@ -127,6 +170,30 @@ export default class AudioReactive {
     // }
   }
 
+  _getMaxAudioPower() {
+    let max = 0;
+
+    for (let i = 0; i < this._frequencyRange; i++) {
+      max += MAX_DECIBELS + i;
+    }
+
+    this.MAX_POWER = (max / this._frequencyRange - 1) / 100;
+    console.log(`Max audio power = ${this.MAX_POWER}`);
+  }
+
+  _setAudioValues() {
+    this.SONG_MIN_POWER = this._minFrequency / this.MAX_POWER;
+    this.SONG_MAX_POWER = this._maxFrequency / this.MAX_POWER;
+    this.SONG_RANGE = this.SONG_MAX_POWER - this.SONG_MIN_POWER;
+
+    console.log(`Song min frequency = ${this._minFrequency}`);
+    console.log(`Song max frequency = ${this._maxFrequency}`);
+    console.log(`Song frequency range = ${this.SONG_RANGE}`);
+
+    this._studingAudio = false;
+    this._loadAudioTrack();
+  }
+
   showStats() {
     if (!this.stats) {
       this.stats = new Stats();
@@ -139,9 +206,12 @@ export default class AudioReactive {
     this.stats.dom.parentNode.removeChild(this.stats.dom);
   }
 
-  resize(w, h) {
-    this._width = w || this._element.width;
-    this._height = h || this._element.height;
+  resize(width, height) {
+    this._width = width;
+    this._height = height;
+
+    this._element.width = this._width;
+    this._element.height = this._height;
   }
 
   destroy() {
